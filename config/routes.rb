72ec6ba59e09a3505df9a603ac1e6a33ac5a7d5d
Takamitsu Endo <ryukau@gmail.com>
2023-03-02 12:53:02 +0900

    Add functionality to destroy log

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   root "home#top"
 
-  resources :posts, only: [:new, :create, :show, :index]
+  resources :posts, only: [:new, :create, :show, :index, :destroy]
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
